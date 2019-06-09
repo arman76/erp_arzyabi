d11e2d13609d12ce344093ef1fdbf7d3f490c3c8
arman76 <armgham92@gmail.com>
2019-06-10 03:31:46 +0430

    Add files via upload

---
+++ app.py
@@ -0,0 +1,161 @@
+# -*- coding: utf-8 -*-
+from telegram import ReplyKeyboardMarkup
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+import scrp
+import _thread
+
+import os
+
+
+'''
+os.system('wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz')
+os.system('tar -zxvf geckodriver-v0.11.1-linux64.tar.gz')
+os.system('wget https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2')
+os.system('tar xjf phantomjs-2.1.1-linux-x86_64.tar.bz2')
+cwd = os.getcwd()
+path = cwd + '/phantomjs-2.1.1-linux-x86_64/bin'
+os.environ["PATH"] += os.pathsep + path
+os.environ["PATH"] += os.pathsep + cwd
+'''
+
+
+
+
+CHOOSING, TYPING_REPLY, TYPING_CHOICE, USERPASS, START_TIME, FINISH_TIME, COMMENTS, LESSON, PROFESSOR, CHOOSING_NOMRE, DATE = range(
+    11)
+
+
+
+
+
+reply_keyboard = [['فرستادن نام کاربری و کلمه عبور (username, password)'],
+                  ['شروع']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def start(bot, update):
+    update.message.reply_text(
+        'سلام! کار من اینه که با استفاده از یوزرنیم و پسورد erp وارد سامانه بشم'
+        ' و فرم ارزشیابی و انجام بدم! *فقط یه نکته مهم واسه هر دفعه که باید نمره بدی ۲۰ ثانیه وقت داری*',
+        reply_markup=markup
+    )
+    return CHOOSING
+
+
+def user_pass(bot, update, user_data):
+    user_data['choice'] = 'username'
+    update.message.reply_text('خب {} خودتو بده:'.format('نام کاربری'))
+    return USERPASS
+
+
+def received_userpass(bot, update, user_data):
+    user_data['nomre'] = 0
+    with open('n.txt', 'r') as f:
+        NUM = int(f.read())
+    print(NUM)
+    if NUM == 1:
+        update.message.reply_text('الان یه نفر دیگه داره از بات استفاده میکنه بعدا دوباره امتحان کن',
+                                  reply_markup=markup)
+        return CHOOSING
+    text = update.message.text
+    category = user_data['choice']
+    user_data[category] = text
+    if category == 'username':
+        update.message.reply_text('خب {} خودتو بده:'.format('کلمه عبور'))
+        user_data['choice'] = 'password'
+        return USERPASS
+    del user_data['choice']
+    if 'time_table' in user_data:
+        update.message.reply_text('خب الان شروع میکنم!')
+        _thread.start_new_thread(scrp.main, (user_data, bot, update))
+        del user_data['time_table']
+        bot.send_message(chat_id=update.message.chat.id, text='یه ذره صبر کن!')
+        return CHOOSING
+    update.message.reply_text('خب اطلاعات گرفته شد! الان میتونی شروع کنی!',
+                              reply_markup=markup)
+    return CHOOSING
+
+
+def start_scrp(bot, update, user_data):
+    if 'username' not in user_data:
+
+        update.message.reply_text('اول باید نام کاربری و کلمه عبور رو بفرستی!')
+        bot.send_message(chat_id=update.message.chat_id, text='خب {} خودتو بده:'.format('نام کاربری'))
+        user_data['choice'] = 'username'
+        return USERPASS
+
+    with open('n.txt', 'r') as f:
+        NUM = int(f.read())
+    print(NUM)
+    if NUM == 1:
+        update.message.reply_text('الان یه نفر دیگه داره از بات استفاده میکنه بعدا دوباره امتحان کن',
+                                  reply_markup=markup)
+        return CHOOSING
+    _thread.start_new_thread(scrp.main, (user_data, bot, update))
+    bot.send_message(chat_id=update.message.chat.id, text='یه ذره صبر کن!')
+    return CHOOSING
+
+
+def received_nomre(bot, update, user_data):
+    nomre = update.message.text
+    nomre = 20 - int(nomre)
+    user_data['nomre'] = nomre
+
+    return CHOOSING
+
+
+def restart(bot, update, user_data):
+    user_data.clear()
+    update.message.reply_text('اطلاعاتت پاک شد.', reply_markup=markup)
+
+
+def unknown(bot, update):
+    update.message.reply_text('ورودی یا دستور نامعتبر!', reply_markup=markup)
+    return CHOOSING
+
+
+def main():
+    updater = Updater('590619031:AAFVHZjrOKusFYtBiaDzTAN9fG7mVlA4vaI')
+
+    dp = updater.dispatcher
+    restart_command_handler = CommandHandler('stop', restart, pass_user_data=True)
+    dp.add_handler(restart_command_handler)
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start), MessageHandler(Filters.text, start)],
+
+        states={
+            CHOOSING: [RegexHandler('^.*\(username\, password\)$',
+                                    user_pass,
+                                    pass_user_data=True),
+                       RegexHandler('^شروع$',
+                                    start_scrp,
+                                    pass_user_data=True),
+                       CommandHandler('start',
+                                      start),
+                       MessageHandler(Filters.all,
+                                      unknown),
+                       MessageHandler(Filters.text,
+                                      received_nomre,
+                                      pass_user_data=True),
+                       ],
+            USERPASS: [MessageHandler(Filters.text,
+                                      received_userpass,
+                                      pass_user_data=True)],
+
+        },
+
+        fallbacks=[]
+    )
+
+    dp.add_handler(conv_handler)
+
+    updater.start_polling()
+
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
+
+# '517255695:AAFSQ549HEYNGhDCT3iC2dLgst1w5YPLOOA'
---
+++ scrp.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium import webdriver
+from time import sleep
+from telegram import ReplyKeyboardMarkup
+import app
+
+
+reply_keyboard = [['20'],
+                  ['19'],
+                  ['18'],
+                  ['17'],
+                  ['16'],
+                  ['15'],
+                  ['14'],
+                  ['13'],
+                  ['12']]
+markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True)
+
+
+def main(user_data, bot, update):
+    with open('n.txt', 'w') as f:
+        f.write('1')
+    driver = webdriver.PhantomJS()
+    try:
+        driver.get("http://erp.guilan.ac.ir/Dashboard.aspx")
+        if 'erp.guilan.ac.ir/GoToDashboard.aspx' in driver.current_url:
+            driver.find_element_by_class_name('refreshDash').click()
+
+        wait = WebDriverWait(driver, 10)
+        elem = wait.until(ec.presence_of_element_located((By.PARTIAL_LINK_TEXT, 'ورود به س')))
+        elem.click()
+        elem = wait.until(ec.presence_of_element_located((By.ID, 'iframe_040101')))
+
+        driver.get(elem.get_property('src'))
+        elem = driver.find_element_by_name('SSMUsername_txt')
+        elem.send_keys(user_data['username'])
+
+        elem = driver.find_element_by_name('SSMPassword_txt')
+        elem.send_keys(user_data['password'] + Keys.ENTER)
+        elem = wait.until(ec.presence_of_element_located((By.ID, 'FLASH_URL_TAB_ID')))
+        elem.click()
+
+        elem22 = wait.until(ec.presence_of_element_located((By.ID, 'iframe_FLASH_URL_TAB_ID')))
+        driver.get(elem22.get_property('src'))
+        elemfs = wait.until(ec.presence_of_all_elements_located((By.CLASS_NAME, 'Eval-grid')))
+        while True:
+            elemfs[0].click()
+            sleep(2)
+            tabs = driver.window_handles
+            driver.close()
+            driver.switch_to.window(tabs[1])
+            elems = wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, 'input[type="button"]')))
+            while True:
+                elems[0].click()
+                sleep(2)
+                tabs = driver.window_handles
+                print(tabs)
+                driver.switch_to.window(tabs[1])
+                elem = wait.until(ec.presence_of_element_located((By.ID, 'InfoLbl')))
+                print(elem.text)
+                bot.send_message(chat_id=update.message.chat.id, text= elem.text + '\n نمره رو بزن:' , reply_markup=markup)
+                t = 0
+                print('tt')
+                while t <= 20:
+                    print('t1')
+                    if user_data['nomre'] == 0:
+                        if t == 20:
+                            reply_keyboard2 = [['فرستادن نام کاربری و کلمه عبور (username, password)'],
+                                               ['شروع']]
+                            markup2 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True)
+
+                            bot.send_message(chat_id=update.message.chat.id, text='خب تایمت تموم شد! بای بای!', reply_markup=markup2)
+                            driver.close()
+                            with open('n.txt', 'w') as f:
+                                f.write('0')
+                            exit()
+                        sleep(1)
+                        t += 1
+                    else:
+                        break
+                nomre = user_data['nomre']
+                print(nomre)
+                els = wait.until(ec.presence_of_all_elements_located((By.TAG_NAME, 'input')))
+                for el in els:
+                    if el.get_attribute('id')[:3] == 'rb' + str(nomre):
+                        el.click()
+                elem = wait.until(ec.presence_of_element_located((By.ID, 'btnContinue')))
+                elem.click()
+                sleep(2)
+                driver.close()
+                driver.switch_to.window(tabs[0])
+                user_data['nomre'] = 0
+                if len(elems) == 1:
+                    break
+                elems = wait.until(ec.presence_of_all_elements_located((By.CSS_SELECTOR, 'input[type="button"]')))
+            if len(elemfs) == 1:
+                break
+            driver.get(elem22.get_property('src'))
+            elemfs = wait.until(ec.presence_of_all_elements_located((By.CLASS_NAME, 'Eval-grid')))
+        bot.send_message(chat_id=update.message.chat.id, text='خب تموم شششددددد !!!!!')
+        driver.close()
+        with open('n.txt', 'w') as f:
+            f.write('0')
+        exit(0)
+
+    except Exception as e:
+        print(e.args)
+        print(user_data)
+        reply_keyboard2 = [['فرستادن نام کاربری و کلمه عبور (username, password)'],
+                          ['شروع']]
+        markup2 = ReplyKeyboardMarkup(reply_keyboard2, one_time_keyboard=True)
+
+        bot.send_message(chat_id=update.message.chat.id, text='به ارور برخوردیم! شاید فرم ارزشیابی تموم شده باشه شایدم یوزر پسورد اشتباه باشه', reply_markup=markup2)
+        try:
+            driver.close()
+            with open('n.txt', 'w') as f:
+                f.write('0')
+        except Exception as e:
+            print(e.args)
+            pass
